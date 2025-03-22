40c9d29d52ba3d0440edac5d8309b79458eb2017
Fey C <lordseancap@gmail.com>
2025-03-22 00:12:17 -0700

    Update AtlyssToolsResourceAPI.cs

--- Utility/AtlyssToolsResourceAPI.cs
+++ Utility/AtlyssToolsResourceAPI.cs
@@ -2,40 +2,41 @@ using UnityEngine;
 
 namespace AtlyssTools.Utility;
 
-// this will redirect the ResourcesAPI to try and use the AtlyssTools loader, before falling back to the default Unity loader
+// WIP
+
 public class AtlyssToolsResourceAPI : UnityEngine.ResourcesAPI
 {
     // for now just log the call and pass it through
     public override Object Load(string path, System.Type type)
     {
-        Debug.Log($"AtlyssToolsResourceAPI.Load({path}, {type})");
+        //Debug.Log($"AtlyssToolsResourceAPI.Load({path}, {type})");
         return base.Load(path, type);
     }
     
     // for now just log the call and pass it through
     public override Object[] LoadAll(string path, System.Type type)
     {
-        Debug.Log($"AtlyssToolsResourceAPI.LoadAll({path}, {type})");
+        //Debug.Log($"AtlyssToolsResourceAPI.LoadAll({path}, {type})");
         return base.LoadAll(path, type);
     }
     
     // for now just log the call and pass it through
     public override Shader FindShaderByName(string name)
     {
-        Debug.Log($"AtlyssToolsResourceAPI.FindShaderByName({name})");
+        //Debug.Log($"AtlyssToolsResourceAPI.FindShaderByName({name})");
         return base.FindShaderByName(name);
     }
     
     // for now just log the call and pass it through
     public override ResourceRequest LoadAsync(string path, System.Type type)
     {
-        Debug.Log($"AtlyssToolsResourceAPI.LoadAsync({path}, {type})");
+        //Debug.Log($"AtlyssToolsResourceAPI.LoadAsync({path}, {type})");
         return base.LoadAsync(path, type);
     }
     
     public override void UnloadAsset(Object assetToUnload)
     {
-        Debug.Log($"AtlyssToolsResourceAPI.UnloadAsset({assetToUnload})");
+        //Debug.Log($"AtlyssToolsResourceAPI.UnloadAsset({assetToUnload})");
         base.UnloadAsset(assetToUnload);
     }
 }
